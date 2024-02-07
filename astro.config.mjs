6db5d08c4705b978bdd4afa77cfa260892fd6fb4
th7mo <thimo@th7mo.com>
2024-02-07 09:09:16 +0100

    feat: use markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    build: {
-        format: 'file'
-    }
+  markdown: {
+    syntaxHighlight: false
+  }
 });
