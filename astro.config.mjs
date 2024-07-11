f2a98d3ea4601378078c58868839455eedde795f
th7mo <thimo@th7mo.com>
2024-07-11 20:18:25 +0200

    possible-fix: redirect to correct domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://th7mo.com',
   markdown: {
     shikiConfig: {
       theme: 'css-variables',
