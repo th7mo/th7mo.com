070b09fb3c0772eed75f4abc361ec2bea29eb3b0
th7mo <thimo@th7mo.com>
2024-07-18 11:42:59 +0200

    feat: remove comments

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,5 @@
-// Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
-// Define a `type` and `schema` for each collection
+
 const notes = defineCollection({
     type: 'content',
     schema: z.object({
@@ -9,7 +8,7 @@ const notes = defineCollection({
       isPublic: z.boolean()
     })
 });
-// Export a single `collections` object to register your collection(s)
+
 export const collections = {
   notes,
 };
