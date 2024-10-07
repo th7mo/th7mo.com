a0fd49a1c6d5b2672b900b3e66b60e3ae1298566
th7mo <thimo@th7mo.com>
2024-10-07 14:15:56 +0200

    revert: this is really bad stuff:

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://th7mo.com",
-  build: {
-    format: "file"
-  },
   markdown: {
     syntaxHighlight: false
   }
