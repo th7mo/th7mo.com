95c62d5666441aec92e9145c0658cd0016a7ee82
th7mo <thimo@th7mo.com>
2024-10-07 14:27:39 +0200

    feat: enforce consistent slashes between dev and prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://th7mo.com",
+  trailingSlash: "always",
   markdown: {
     syntaxHighlight: false
   }
