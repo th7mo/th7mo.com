34869b092f5876907267a2eb2fb9e44dbe9618e1
th7mo <thimo@th7mo.com>
2024-10-07 14:14:24 +0200

    wip: try

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://th7mo.com",
+  build: {
+    format: "file"
+  },
   markdown: {
     syntaxHighlight: false
   }
