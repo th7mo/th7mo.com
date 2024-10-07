1966ac93b70bf317b28345b2c2ef63c9b77b5736
th7mo <thimo@th7mo.com>
2024-10-07 16:28:30 +0200

    feat: try cleaner build url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://th7mo.com",
-  trailingSlash: "always",
+  build: {
+    format: "file"
+  },
   markdown: {
     syntaxHighlight: false
   }
