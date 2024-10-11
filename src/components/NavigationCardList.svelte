f4117065600ed9bbebb9e8b377237086e192b6bc
th7mo <thimo@th7mo.com>
2024-10-11 08:38:38 +0200

    fix: margin bottom is ignored in PROD but not in DEV :/

--- src/components/NavigationCardList.svelte
+++ src/components/NavigationCardList.svelte
@@ -6,7 +6,7 @@
     ol {
         display: grid;
         padding: 0;
-        margin: 0;
+        margin-bottom: 0 !important;
         gap: var(--spacing-md);
         grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
         list-style: none;
