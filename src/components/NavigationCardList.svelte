70426774b170caa9d1009fea687b2a1a6c89ab0b
th7mo <thimo@th7mo.com>
2024-10-11 08:27:48 +0200

    feat: different layout for index page

--- src/components/NavigationCardList.svelte
+++ src/components/NavigationCardList.svelte
@@ -6,6 +6,7 @@
     ol {
         display: grid;
         padding: 0;
+        margin: 0;
         gap: var(--spacing-md);
         grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
         list-style: none;
