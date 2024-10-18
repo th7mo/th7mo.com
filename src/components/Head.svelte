a96747b8505abd8a5cf128f772d08fa46c789210
th7mo <thimo@th7mo.com>
2024-10-18 20:47:36 +0200

    feat: use different icon

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{title}</title>
-    <link rel="icon" href="/images/note.svg">
+    <link rel="icon" href="/images/webring.svg">
     <link
         rel="preload"
         href="/fonts/iosevka-400-monospace.woff2"
